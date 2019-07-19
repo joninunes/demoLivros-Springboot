28e5812ab3fc8e040885d0a9f875ce0f47940b93
Joni Nunes <joninunes@gmail.com>
2019-07-19 10:35:37 -0400

    Ajuste nos titulos dos livros

--- src/main/java/py/com/datapar/demoLivros/model/LivroService.java
+++ src/main/java/py/com/datapar/demoLivros/model/LivroService.java
@@ -1,7 +1,5 @@
 package py.com.datapar.demoLivros.model;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,18 +8,24 @@ public class LivroService {
 
 	@Autowired
 	private LivroRepository livroRepo;
-	
 
-	
 	public void carregarLivros() {
-		Livro l1 = new Livro("HISTORIA DO TIRIRICA", "AAAAAAAAAA", "1990");
+		Livro l1 = new Livro("Anna Kariênina, de Lev Tolstói",
+				"Com absoluta maestria, Tolstói conduz o leitor por um salão repleto de música, perfumes, vestidos de renda, num ambiente de imagens vívidas e quase palpáveis que têm como pano de fundo a Rússia czarista.",
+				"1875");
 		livroRepo.save(l1);
-		Livro l2 = new Livro("BBBBBBBBBB", "AAAAAAAAAA", "1990");
+		Livro l2 = new Livro("Madame Bovary, de Gustave Flaubert",
+				"A história faz um ataque à burguesia, desmoralizando-a com a descrição exuberante de sua banalidade.",
+				null);
 		livroRepo.save(l2);
-		Livro l3 = new Livro("CCCCCC", "AAAAAAAAAA", "1990");
+		Livro l3 = new Livro("Guerra e Paz, de Lev Tolstói",
+				"‘Guerra e Paz’ descreve a campanha de Napoleão Bonaparte na Rússia e estende-se até o ano de 1820. Baseado em meticulosa e exaustiva pesquisa",
+				null);
 		livroRepo.save(l3);
-		Livro l4 = new Livro("HISTORIA DO DDDDDDD", "AAAAAAAAAA", "1990");
+		Livro l4 = new Livro("Lolita, de Vladímir Nabókov",
+				"olêmico, irônico, tocante, narra o amor obsessivo de Humbert Humbert, um cínico intelectual de meia-idade, por Dolores Haze, Lolita, 12 anos, uma ninfeta que inflama suas loucuras e seus desejos mais agudos.",
+				null);
 		livroRepo.save(l4);
 	}
-	
+
 }
